14b1195c5cddc61a1837d5cb2a5602bf37992e89
Kulpot <kulpotwahu@gmail.com>
2024-02-05 18:04:20 +0800

    ...

--- const pointers/CppScratchPad.cpp
+++ const pointers/CppScratchPad.cpp
@@ -2,34 +2,73 @@
 using std::cout;
 using std::endl;
 
+//------------------------- const pointers -----------------------------------------------------------------
+//ref link:https://www.youtube.com/watch?v=BimM1lLeZg8&list=PLRwVmtr-pp070AKU7G71q8XyCRttsWIvi&index=2&t=1s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //------------------ C++ const functions (const correctness) --------------------------
 //ref link:https://www.youtube.com/watch?v=ehJKvbbe9hk
 
 //const int MY_INT = 5;
 
-class Cow
-{
-	int mooCount;
-	//const int mooCount;
-
-public:
-	Cow() { mooCount = 0; }
-	//void moo() { cout << mooCount++ << endl; }		//none-const func	, modifies int mooCount;
-	//void saySomething() { cout << "Hello" << endl; }	// none modify int mooCount;
-	
-	//void moo() const { cout << mooCount++ << endl; }		//error: const cant modify mooCount to mooCount++
-	void moo() const { cout << mooCount << endl; }			
-	void saySomething() const { cout << "Hello" << endl; }
-};
-
-void main()
-{
-	//Cow c;
-	const Cow c;
-	c.moo();
-	c.moo();
-	c.saySomething();
-	c.moo();
-
-	//cout << MY_INT << endl;
-}
+//class Cow
+//{
+//	int mooCount;
+//	//const int mooCount;
+//
+//public:
+//	Cow() { mooCount = 0; }
+//	//void moo() { cout << mooCount++ << endl; }		//none-const func	, modifies int mooCount;
+//	//void saySomething() { cout << "Hello" << endl; }	// none modify int mooCount;
+//	
+//	//void moo() const { cout << mooCount++ << endl; }		//error: const cant modify mooCount to mooCount++
+//	void moo() const { cout << mooCount << endl; }			
+//	void saySomething() const { cout << "Hello" << endl; }
+//};
+//
+//void main()
+//{
+//	//Cow c;
+//	const Cow c;
+//	c.moo();
+//	c.moo();
+//	c.saySomething();
+//	c.moo();
+//
+//	//cout << MY_INT << endl;
+//}
